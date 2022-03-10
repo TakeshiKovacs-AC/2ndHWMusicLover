fc5fcba0c01b92f6583547d18f65d5a40945dc69
Takeshi Kovacs <antonbasket@list.ru>
2022-03-11 00:35:40 +1000

    Исправлено согласно рекомендациям Максима

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,10 @@
 fun main() {
-    val buySum = 550.00
-    val totalSum = if (buySum > 10_001) buySum * 0.95 * 100 else if (buySum in 1_001.0..10_000.0) (buySum - 100) * 100
-          else (buySum * 100)
-    val musicLoverOrNot = true
-    val finalAmount = if (musicLoverOrNot) totalSum * 0.99 else totalSum
-    val finalAmountInteger = finalAmount / 100
-    println("Общая сумма покупки - ${finalAmountInteger.toInt()} руб. ${finalAmount.toInt() % 100} коп.")
+    val buySum = 14000.00
+    val totalSum = (if (buySum > 10_001) buySum * 0.95
+        else if (buySum > 1_001.0) buySum - 100
+        else buySum) * 100
+    val musicLoverOrNot = false
+    val finalAmount = (if (musicLoverOrNot) totalSum * 0.99 else totalSum) / 100
+
+    println("Общая сумма покупки - ${finalAmount.toInt()} руб. ${finalAmount.toInt() % 100} коп.")
 }
