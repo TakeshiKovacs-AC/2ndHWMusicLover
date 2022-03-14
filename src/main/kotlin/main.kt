baabdaae9b43b954bd1c73a9fde9ca9983926554
Takeshi Kovacs <antonbasket@list.ru>
2022-03-14 16:21:46 +1000

    Доработка2: исправлено деление на 100, вывод

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,10 @@
 fun main() {
-    val buySum = 14000.00
+    val buySum = 18_603.48
     val totalSum = (if (buySum > 10_001) buySum * 0.95
         else if (buySum > 1_001.0) buySum - 100
         else buySum) * 100
-    val musicLoverOrNot = false
-    val finalAmount = (if (musicLoverOrNot) totalSum * 0.99 else totalSum) / 100
+    val musicLoverOrNot = true
+    val finalAmount = (if (musicLoverOrNot) totalSum * 0.99 else totalSum).toInt()
 
-    println("Общая сумма покупки - ${finalAmount.toInt()} руб. ${finalAmount.toInt() % 100} коп.")
+    println("Общая сумма покупки - ${(finalAmount / 100)} руб. ${(finalAmount % 100)} коп.")
 }
